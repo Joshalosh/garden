0996a546ef3b28526574fcf596a3beee6fc28a25
Joshalosh <josh.jessopsmith@gmail.com>
2024-09-29 19:14:28 +1000

    Set up the repo

---
+++ build.bat
@@ -0,0 +1,40 @@
+@echo off
+
+ctime -begin generator.ctm
+
+set CompilerFlags= /Zi /MDd /FC /nologo
+REM set LinkerFlags=-subsystem:Console
+
+IF NOT EXIST build mkdir build
+
+pushd build
+
+cl %CompilerFlags% /c /D PLATFORM_DESKTOP /I..\external\Raylib\external\glfw\include ^
+    ..\external\Raylib\rcore.c ^
+    ..\external\Raylib\rmodels.c ^
+    ..\external\Raylib\raudio.c ^
+    ..\external\Raylib\rglfw.c ^
+    ..\external\Raylib\rshapes.c ^
+    ..\external\Raylib\rtext.c ^
+    ..\external\Raylib\rtextures.c ^
+    ..\external\Raylib\utils.c
+
+cl %CompilerFlags% ^
+    ..\garden.cpp ^
+    rcore.obj ^
+    rmodels.obj ^
+    raudio.obj ^
+    rglfw.obj ^
+    rshapes.obj ^
+    rtext.obj ^
+    rtextures.obj ^
+    utils.obj ^
+    /I ..\include/ /link /FORCE:MULTIPLE -incremental:no %LinkerFlags% -out:garden.exe ^
+    Gdi32.lib ^
+    winmm.lib ^
+    user32.lib ^
+    shell32.lib
+
+popd
+
+ctime -end garden.ctm
