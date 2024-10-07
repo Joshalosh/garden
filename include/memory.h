e985dae69c810db87f7d2278ee771bb5f938291b
Joshalosh <josh.jessopsmith@gmail.com>
2024-10-08 08:01:42 +1100

    Add arena init function

--- include/memory.h
+++ include/memory.h
@@ -4,3 +4,9 @@ struct Memory_Arena {
     size_t  used;
     u8     *base;
 };
+
+void ArenaInit(Memeory_Arena *arena, size_t size void *base) {
+    arena->size = size;
+    arena->used = 0;
+    arena->base = (u8 *)base;
+}
