3d994dad11e2600a6eca5f8997f29c58036c7a3d
Joshalosh <josh.jessopsmith@gmail.com>
2025-04-27 18:22:02 +1000

    Increase frame speed

--- include/garden.h
+++ include/garden.h
@@ -7,7 +7,7 @@
 #define MB(x) x*1024ULL*1024ULL
 #define ARENA_SIZE MB(500)
 #define ARRAY_COUNT(array) (sizeof(array) / sizeof((array)[0]))
-#define FRAME_SPEED 4
+#define FRAME_SPEED 8
 const int base_screen_width  = 320;
 const int base_screen_height = 320; //180;
 
